b25a9c8fd4d79f78ab098be8c2b5bc78bd53d04e
cosperec <cosperec@WEB.crdp45.lan>
2015-08-27 16:21:17 +0200

    test

---
+++ js/function.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$( document ).ready(function() {
+  $('.menu').click (function(){
+      $(this).css('height','100%');
+      $(this).css('z-index','20');
+  });
+});
