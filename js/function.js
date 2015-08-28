7d4250ba21068e80fe3cc8f33a71ec2d1f3ea146
cosperec <cosperec@WEB.crdp45.lan>
2015-08-28 13:19:35 +0200

    test

--- js/function.js
+++ js/function.js
@@ -6,8 +6,46 @@
 
 
 $( document ).ready(function() {
-  $('.menu').click (function(){
-      $(this).css('height','100%');
+  var menuName;
+  
+    $('.menu').click (function(){
+      menuName=$(this).attr('id');
       $(this).css('z-index','20');
+        $( this ).animate({
+            top : 0,
+            height: "100%"
+        }, 1000, function() {
+           
+        });
+  });
+  $('button').click (function(e){
+       e.stopPropagation(); //evite la propagation au parent
+       
+       switch(menuName){
+           case 'menu1':          
+            $( this ).parent().animate({
+              top : 0,
+              height: "33%"
+            }, 1000, function() {
+                $(this).css('z-index','1');
+            });
+            break;
+             case 'menu2':          
+            $( this ).parent().animate({
+              top : "33%",
+              height: "33%"
+            }, 1000, function() {
+                $(this).css('z-index','1');
+            });
+            break;
+             case 'menu3':          
+            $( this ).parent().animate({
+              top : "66%",
+              height: "34%"
+            }, 1000, function() {
+                $(this).css('z-index','1');
+            });
+            break;
+        }
   });
 });
